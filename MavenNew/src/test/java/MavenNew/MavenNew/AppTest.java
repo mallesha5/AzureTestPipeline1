c98f8b0df386f1d431c3ac2891770a838d278803
Mallesh <Mallesh@DESKTOP-6U2ELF0.mshome.net>
2022-11-02 23:20:47 +0530

    initial push

---
+++ MavenNew/src/test/java/MavenNew/MavenNew/AppTest.java
@@ -0,0 +1,25 @@
+package MavenNew.MavenNew;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("hello world");
+    }
+    
+    
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+       System.out.println("hello world2");
+    }
+}
